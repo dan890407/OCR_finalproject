b3c53ba511c03365fc7cc1715a41e4233f73d01e
bojun <950094@hhups.tp.edu.tw>
2021-03-06 00:13:10 +0800

    '3/5'

--- OCR/ocr_lib.py
+++ OCR/ocr_lib.py
@@ -1,68 +1,92 @@
 import win32gui
+import pyautogui
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import *
 from PIL import ImageGrab,Image,ImageEnhance
 import win32con
 import time
-import sys
+import sys,os
 import pytesseract
+class project :
+    def __init__(self,hwnd,filename,left,up,right,down,interval):
+        self.hwnd=hwnd
+        self.filename=filename
+        self.left=left
+        self.up=up
+        self.right=right
+        self.down=down
+        self.interval=interval
 
-def catch(ipclass,name):            #取得視窗代碼 ipclass=類名 name=標題
-    hwnd = win32gui.FindWindow(ipclass,name)
-    return hwnd
+    def autofetch(self):
+        ipclass = win32gui.GetClassName(self.hwnd)
+        if ipclass=="Chrome_WidgetWin_1":
+            self.web_screenshot()
+        else:
+            self.app_screenshot()
+        self.divid()
+        self.ocr()
+        self.merge()
 
+    def web_screenshot(self):            #web截圖 圖名稱screenshot
+        win32gui.SetForegroundWindow(self.hwnd)
+        img = ImageGrab.grab()
+        img.save("./screenshot/temporary.jpg")
 
-def web_screenshot(hwnd,screenshot):            #web截圖 圖名稱screenshot
-    win32gui.SetForegroundWindow(hwnd)
-    img = ImageGrab.grab()
-    img.save(screenshot+".jpg")
+    def app_screenshot(self):            #應用程式截圖 圖名稱screenshot
+        app = QApplication(sys.argv)
+        screen = QApplication.primaryScreen()
+        img = screen.grabWindow(self.hwnd).toImage()
+        img.save("./screenshot/temporary.jpg")
 
+    def divid(self):      #切割成特定範圍的圖片
+        img = Image.open("./screenshot/temporary.jpg")
+        new_mg = img.crop((self.left,self.up,self.right,self.down))
+        enh_con = ImageEnhance.Contrast(new_mg)
+        contrast=2
+        image_contrasted = enh_con.enhance(contrast)
+        image_contrasted.save("./screenshot/"+self.filename+".jpg")
+        os.remove("./screenshot/temporary.jpg")
 
-def app_screenshot(hwnd,screenshot):            #應用程式截圖 圖名稱screenshot
-    app = QApplication(sys.argv)
-    screen = QApplication.primaryScreen()
-    img = screen.grabWindow(hwnd).toImage()
-    img.save(screenshot+".jpg")
-
-
-def divid(img,left,up,right,down,newscreenshot):      #切割成特定範圍的圖片
-    new_img = Image.open(img)
-    new_mg = new_img.crop((left,up,right,down))
-    enh_con = ImageEnhance.Contrast(new_mg)
-    contrast=2
-    image_contrasted = enh_con.enhance(contrast)
-    image_contrasted.save(newscreenshot+".jpg")
-
-
-def merge(new_text,all_text):             #將newtext合併到alltext
-    f = open(all_text,'r',encoding="utf-8")   
-    lines = f.readlines()
-    last_line = lines[-1]
-    f.close()
+    def ocr(self):                           #ocr影像辨識
+        test_img = Image.open("./screenshot/"+self.filename+".jpg")
+        newtext = pytesseract.image_to_string(test_img, lang='chi_tra+equ+eng')
+        fixed_text = newtext.strip()
+        self.text=fixed_text
     
-    k=open(new_text,'r',encoding="utf-8") 
-    c=open(all_text,'a+',encoding="utf-8")          
-    check = 0
-    for line in k.readlines():
-        if check == 1:
-            c.write(line)
-        if line == last_line:
-            check = 1
-    k.close()
-    if check==0:
-        k=open(new_text,'r',encoding="utf-8")
-        for line in k.readlines():
-            c.write(line)
-
-
-def ocr(img):                           #ocr影像辨識
-    test_img = Image.open(img)
-    text = pytesseract.image_to_string(test_img, lang='chi_tra+equ+eng')
-    print(text)
-    return text
-
-
-def write(text,place):                #寫檔案入place
-    with open(place,'w',encoding='utf-8') as f:
-        fixed_text = text.strip()
-        f.writelines(fixed_text)
+    def merge(self):             #合併
+        temporary='./text_file/temporary'
+        goal = './text_file/'+self.filename
+        with open(temporary,'w',encoding='utf-8') as t:
+            t.writelines(self.text)
+        with open(goal,'r',encoding='utf-8') as f:
+            lines = f.readlines()
+        t = open(temporary,'r',encoding='utf-8')
+        f = open(goal,'a+',encoding="utf-8")   
+        if os.path.getsize(goal):
+            last_line = lines[-1]
+            print(last_line)
+            check = 0
+            for line in t.readlines():
+                if check == 1:
+                    if line.split():
+                        f.writelines(line)
+                    else:
+                        f.writelines("")
+                if line == last_line:
+                    check = 1
+            if check==0:
+                for line in t.readlines():
+                    if line.split():
+                        f.writelines(line)
+                    else:
+                        f.writelines("")
+        else:
+            for line in t.readlines():
+                if line.split():
+                    f.writelines(line)
+                else:
+                    f.writelines("")
+        t.close()
+        f.close()
+        os.remove(temporary)
+    
--- OCR/test.py
+++ OCR/test.py
@@ -1,7 +1,7 @@
 from ocr_lib import *
 
+test=project(0xa0a9e,"desktop",500,251,1000,889,5) 
+run=True
 
-hwnd = catch("HwndWrapper[Bluestacks.exe;;ab47773c-2155-41f2-893b-7f8210ebb9a0]","BlueStacks")
-print(hex(hwnd))
-app_screenshot(hwnd,"./screenshot/bluestack")
-divid("./screenshot/bluestack.jpg",683,259,1189,663,"./screenshot/newbluestack")
+test.autofetch()    
+time.sleep(test.interval) 
