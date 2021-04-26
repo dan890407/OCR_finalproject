8f78b7b6ed86970f2a38a40157e1c316b7c211f8
bojun <950094@hhups.tp.edu.tw>
2021-04-26 22:47:48 +0800

    '4/26_10:47'

--- OCR/house_web/house_web.py
+++ OCR/house_web/house_web.py
@@ -5,11 +5,18 @@ import webbrowser,json,os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index',methods=['GET','POST'])
 def index():
-    data = "data.json"
-    return render_template('index.html',data=data)
+    if request.method == 'POST':
+        filename = request.form['filename']
+        data1 = filename+"1.json"
+        data2 = filename+"2.json"
+        return render_template('index.html',data1=data1,data2=data2)
+    return render_template('index.html')
 
+@app.route('/')
+def search():
+    return render_template('search.html')
 
 webbrowser.open('http://127.0.0.1:5000')
 app.run()
